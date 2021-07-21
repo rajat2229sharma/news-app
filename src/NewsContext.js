442e0e905b5cfca36b11486ebccfad3c927082a1
rajat <rajat.sharma@destm.com>
2021-07-21 19:12:22 +0530

    changes news cards

--- src/NewsContext.js
+++ src/NewsContext.js
@@ -1,20 +1,16 @@
 import React, { createContext, useEffect, useState} from 'react';
 import axios from 'axios';
-
 export const NewsContext = createContext();
-
 export const NewsContextProvider = (props) => {
     const [data, setData] = useState();
-
     useEffect(() => {
        axios
        .get(
-           `http://hn.algolia.com/api/v1/search?query=bar&restrictSearchableAttributes=url`
+           `http://hn.algolia.com/api/v1/search_by_date?query=...`
         )
         .then((response) => setData(response.data.hits))
         .catch((error) => console.log(error));
     }, [])
-    
     return (
         <NewsContext.Provider value={{ data }}>
             {props.children}
--- src/components/NewsArticle.js
+++ src/components/NewsArticle.js
@@ -4,45 +4,44 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
 const useStyles = makeStyles({
   root: {
     maxWidth: 345,
   },
   text: {
-      margin: '5px 0',
+    margin: '5px 0',
+  },
+  title: {
+    minHeight: 108,
+    fontSize: 17,
   },
 });
-
 export default function NewsArticle({ data }) {
   const classes = useStyles();
-
   return (
     <Card className={classes.root}>
-      <CardActionArea target="_blank" href={data.url}>
+      <CardActionArea target="_blank" href={data.story_url}>
         <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {data.title}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {data.author}
+          <Typography className={classes.title} gutterBottom variant="h6" component="h6">
+            {data.story_title}
           </Typography>
           <Typography className={classes.text} variant="body2" color="textSecondary" component="p">
-            {data.num_comments}
+            {data.author}
           </Typography>
           <Typography className={classes.text} variant="body2" color="textSecondary" component="p">
             {data.objectID}
           </Typography>
           <Typography className={classes.text} variant="body2" color="textSecondary" component="p">
-            {data.points}
-          </Typography>
-          <Typography className={classes.text} variant="body2" color="textSecondary" component="p">
-            {data.relevancy_score}
-          </Typography>
-          <Typography className={classes.text} variant="body2" color="textSecondary" component="p">
             {data.created_at}
           </Typography>
         </CardContent>
+        <CardActions>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
       </CardActionArea>
     </Card>
   );
